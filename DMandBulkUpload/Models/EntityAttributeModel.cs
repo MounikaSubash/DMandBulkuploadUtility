f6550038af9b02f6c562cfca348be0b8dc41dab5
Saravanan Dhandapani <Saravanan.D@datayaan.com>
2023-09-20 18:54:29 +0530

    adding EntityAttributeModel

---
+++ DMandBulkUpload/Models/EntityAttributeModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DMandBulkUpload.Models
+{
+    public class EntityAttributeModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="this field is required")]
+        public string ColumnName { get; set; }
+
+        [Required(ErrorMessage = "this field is required")]
+        public string ColumnDatatype { get; set; }
+     
+        public int length { get; set; }
+        
+        public bool Default { get; set; }
+
+        public DateTime createdDate { get; set; }
+
+        public DateTime updatedDate { get; set; }
+    }
+}
